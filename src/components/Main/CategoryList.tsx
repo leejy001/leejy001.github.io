8558057fc5d7447f6f0f2b394cd09944bf152508
leejy001 <greatness007@naver.com>
2021-12-18 17:07:19 +0900

    Design: 카테고리 디자인 수정 및 마크다운 테스트 파일 작성

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -20,15 +20,18 @@ type GatsbyLinkTypes = {
 function CategoryList({ selectedCategory, categoryList }: CategoryListTypes) {
   return (
     <CategoryListWrapper>
-      {Object.entries(categoryList).map(([name, count]) => (
-        <CategoryItem
-          to={`/categories?category=${name}`}
-          active={name === selectedCategory}
-          key={name}
-        >
-          #{name}({count})
-        </CategoryItem>
-      ))}
+      <Wrapper>
+        {Object.entries(categoryList).map(([name, count]) => (
+          <CategoryItem
+            to={`?category=${name}`}
+            active={name === selectedCategory}
+            key={name}
+          >
+            {name}
+            <span>{count}</span>
+          </CategoryItem>
+        ))}
+      </Wrapper>
     </CategoryListWrapper>
   )
 }
@@ -38,28 +41,40 @@ export default CategoryList
 const CategoryListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 768px;
-  margin: auto;
-  background-color: red;
+  width: 1024px;
+  margin: 15px auto 0;
   @media (max-width: 768px) {
     width: 100%;
-    margin-top: 50px;
-    padding: 0 20px;
   }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
 `
 
 const CategoryItem = styled(({ active, ...props }: GatsbyLinkTypes) => (
   <Link {...props} />
 ))<CategoryItemTypes>`
+  background-color: #08c1ce;
+  border-radius: 5px;
   margin-right: 20px;
-  padding: 5px 0;
-  font-size: 18px;
+  margin-bottom: 10px;
+  padding: 5px;
+  font-size: 16px;
   font-weight: ${({ active }) => (active ? '800' : '400')};
   cursor: pointer;
   &:last-of-type {
     margin-right: 0;
   }
+  span {
+    background-color: white;
+    padding: 0 5px;
+    border-radius: 5px;
+    margin-left: 5px;
+  }
   @media (max-width: 768px) {
-    font-size: 15px;
+    font-size: 14px;
   }
 `
